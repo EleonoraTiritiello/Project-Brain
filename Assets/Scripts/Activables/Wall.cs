7153ff6e0515b7ba9a2c4b31bfacb69ede2c71ae
redd096 <martin.babino96@gmail.com>
2021-02-15 10:04:26 +0100

    creato prefab muro

---
+++ Assets/Scripts/Activables/Wall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : Activable
+{
+    protected override void Active()
+    {
+
+    }
+
+    protected override void Deactive()
+    {
+
+    }
+}
