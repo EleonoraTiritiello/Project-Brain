1c2abb41467f8684d1f0182855bdeca866bcc1aa
redd096 <martin.babino96@gmail.com>
2021-03-18 14:27:58 +0100

    migliorata icona minimappa

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -10,25 +10,24 @@ public class LevelManager : MonoBehaviour
 
     [Header("Minimap")]
     [SerializeField] GameObject MinimapIcon = default;
-    [SerializeField] float prefabScaler = 0.1f;
 
     [Header("Debug")]
     [ReadOnly] public RoomGame currentRoom;
 
-    Transform minimapIcons;
+    Transform minimapIconsParent;
 
     void Awake()
     {
         //create parent icons
-        minimapIcons = new GameObject("Minimap Icons").transform;
+        minimapIconsParent = new GameObject("Minimap Icons").transform;
     }
 
     public GameObject CreateIcon(Vector3 position, float tileSize, int width, int height)
     {
         //instantiate minimap icon
-        GameObject minimapIcon = Instantiate(GameManager.instance.levelManager.MinimapIcon, minimapIcons);
+        GameObject minimapIcon = Instantiate(GameManager.instance.levelManager.MinimapIcon, minimapIconsParent);
         minimapIcon.transform.position = position;
-        minimapIcon.transform.localScale = new Vector3(width * tileSize, 1, height * tileSize) * prefabScaler;
+        minimapIcon.transform.localScale = new Vector3(width * tileSize, height * tileSize, 1);
 
         //deactive by default
         minimapIcon.SetActive(false);
